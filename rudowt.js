ae4f4ca17da6d4fea30ed2037b6e4fefe4adc189
naitikgour2122-arch <naitikgour2122@gmail.com>
2026-08-19 19:37:39 +0530

    Update rudowt.js

--- rudowt.js
+++ rudowt.js
@@ -41,7 +41,7 @@ async function initBot() {
     try {
         const dbClient = new MongoClient(mongoUrl);
         await dbClient.connect();
-        const db = dbClient.db('godffpom');
+        const db = dbClient.db('rudowt');
         collection = db.collection('botStorage');
         console.log("✅ Connected to MongoDB successfully!");
 
