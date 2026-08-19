908e605caacc0c9ecf0abcafa13b649a30111a88
naitikgour2122-arch <naitikgour2122@gmail.com>
2026-08-19 19:52:12 +0530

    Refactor module imports in index.js
    Removed multiple require statements for unused modules and added require for rajwt.js.

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 const TelegramBot = require('node-telegram-bot-api');
 const axios = require('axios');
 const { MongoClient } = require('mongodb');
-require('./mongodbeditor.js');
-require('./zenompom.js');
-require('./godffpom.js');
-require('./rajpom.js');
-require('./ash-rat.js');
+require('./rajwt.js');
 const botToken = "8609915265:AAF7qhBvyR6yla8OGZBoun40b7nhDTnW9P8";
 const adminId = "5291409360";
 const mongoUrl = process.env.MONGO_URL; 
